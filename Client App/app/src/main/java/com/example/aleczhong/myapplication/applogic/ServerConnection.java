fdf31eda7ad331a1cfba015b201c58a7d55bbc88
aids12321 <43422955+aids12321@users.noreply.github.com>
2018-10-09 09:59:16 +1300

    changed ip address
    disregard

--- Client App/app/src/main/java/com/example/aleczhong/myapplication/applogic/ServerConnection.java
+++ Client App/app/src/main/java/com/example/aleczhong/myapplication/applogic/ServerConnection.java
@@ -10,12 +10,8 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-class ServerConnection {
-<<<<<<< HEAD
+class ServerConnection {S
     private static final String SERVER_ADDRESS = "192.168.18.49";
-=======
-    private static final String SERVER_ADDRESS = "192.168.0.11";
->>>>>>> Aidan
     private static final int SERVER_PORT = 8818;
 
     private Socket connection;
