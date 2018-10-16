da592b3925d74dfdd1f3e47c9f26cc772652f410
Aidan Stehbens <a.stehbens@hotmail.com>
2018-10-16 15:16:59 +1300

    disregard

--- Client App/app/src/main/java/com/example/aleczhong/myapplication/applogic/ServerConnection.java
+++ Client App/app/src/main/java/com/example/aleczhong/myapplication/applogic/ServerConnection.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 class ServerConnection {
-    private static final String SERVER_ADDRESS = "192.168.0.3";
+    private static final String SERVER_ADDRESS = "172.28.60.240";
     private static final int SERVER_PORT = 8818;
 
     private Socket connection;
