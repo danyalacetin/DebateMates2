e8fb14b5b296c22cfe0951b1ace9fc9da6dc8ae7
Aidan Stehbens <a.stehbens@hotmail.com>
2018-10-24 23:25:48 +1300

    winlose

--- Client App/app/src/main/java/com/example/aleczhong/myapplication/activities/MatchEndActivity.java
+++ Client App/app/src/main/java/com/example/aleczhong/myapplication/activities/MatchEndActivity.java
@@ -4,22 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.aleczhong.myapplication.R;
 
 public class MatchEndActivity extends AppCompatActivity {
 
+    private TextView display;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_end);
+        display = findViewById(R.id.endGameMessage);
         String winLose = getIntent().getExtras().getString("winlose");
         if ("win".equals(winLose)) showWin();
         else if ("lose".equals(winLose)) showLose();
     }
 
     private void showWin() {
-
+        
     }
 
     private void showLose() {
